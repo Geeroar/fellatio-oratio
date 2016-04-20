98db59e040c67f4e89c718ad068d17ce93b1de12
George Boyle <george@thebuds.net>
2016-04-20 23:18:02 +0200

    Update minor stuff

--- server/main.js
+++ server/main.js
@@ -1,5 +1,13 @@
 import { Meteor } from 'meteor/meteor';
+import { Mongo } from 'meteor/mongo';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+});
+
+Meteor.methods({
+    joinGame: () => {
+    },
+
+    submitAnswer: (answer) => {
+    },
 });
