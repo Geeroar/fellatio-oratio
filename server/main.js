a3b11ca3a3c6d8485dfb5fd37704886a74f565c3
George Boyle <george@thebuds.net>
2016-04-24 15:22:28 +0200

    wippy

--- server/main.js
+++ server/main.js
@@ -6,4 +6,27 @@ Meteor.startup(() => {
 });
 
 Meteor.methods({
+    getGame: () => game,
+
+    joinGame: () => {
+        var activeGame = Games.findOne({state: "WAITING_FOR_START"});
+        if (!activeGame) {
+            activeGame = {
+                state: "WAITING_FOR_START",
+                numberOfPlayers: 1,
+            }
+            id = Games.insert(activeGame);
+            activeGame._id = id;
+        } else {
+            Games.update({_id: activeGame._id}, {$set: {
+                state: "IN_PROGRESS",
+                numberOfPlayers: 2,
+            }});
+            activeGame.state = "IN_PROGRESS";
+            activeGame.numberOfPlayers = 2;
+        }
+        Meteor.publish("active-game", (id) => Games.findOne({_id: id}));
+        console.log("Active Game: " + JSON.stringify(activeGame));
+        return activeGame;
+    },
 });
